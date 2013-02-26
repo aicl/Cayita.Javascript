f54c4b9a9b38869bd2ab1af656251841f76257da
angel ignacio colmenares laguado <angel.ignacio.colmenares@gmail.com>
2013-02-26 12:53:02 -0500

    demo 2

--- src/Cayita.Javascript.DemoForm/DemoForm.cs
+++ src/Cayita.Javascript.DemoForm/DemoForm.cs
@@ -13,9 +13,8 @@ namespace Cayita.Javascript.DemoForm
 		public static void Execute(Element parent)
 		{
 
-			var title=Document.CreateElement("h2");
-			title.Text("Default styles");
-			parent.Append(title);
+			Document.CreateElement("h2").Text("Default styles").AppendTo(parent);
+
 			new Div(null, div=>{
 				div.ClassName="bs-docs-example";
 				new Form(div , f=>{
@@ -76,7 +75,107 @@ namespace Cayita.Javascript.DemoForm
 		bt.JQuery().Click(ev=&gt;{ev.PreventDefault();});
 	});
 });</textarea></div>");
-			}).AppendTo(parent);;
+			}).AppendTo(parent);
+
+			//---------------------------------
+			Document.CreateElement("h2").Text("Inline Form").AppendTo(parent);
+
+			new Div(null, div=>{
+				div.ClassName="bs-docs-example";
+				new Form(div , f=>{
+					f.ClassName="form-inline";
+					new InputText(f, input=>{
+						input.ClassName="input-small";
+						input.SetPlaceHolder("Email");
+					});
+					new InputPassword(f, input=>{
+						input.ClassName="input-small";
+						input.SetPlaceHolder("Password");
+					});
+					new Label(f, lb=>{
+						new InputCheckbox(lb, cb=>{});
+						lb.Append("Remember me");
+						lb.ClassName="checkbox";
+					});
+					new SubmitButton(f, bt=>{
+						bt.Text("Sign in");
+						bt.JQuery().Click(ev=>{ev.PreventDefault();});
+					});
+				});
+				div.Append(@"<div class=""dp-highlighter""><div class=""bar""><div class=""tools""><a href=""#"" onclick=""dp.sh.Toolbar.Command('ViewSource',this);return false;"">view plain</a><a href=""#"" onclick=""dp.sh.Toolbar.Command('CopyToClipboard',this);return false;"">copy to clipboard</a><a href=""#"" onclick=""dp.sh.Toolbar.Command('PrintSource',this);return false;"">print</a><a href=""#"" onclick=""dp.sh.Toolbar.Command('About',this);return false;"">?</a></div></div><ol start=""1"" class=""dp-c""><li class=""alt""><span><span class=""keyword"">new</span><span>&nbsp;Form(div&nbsp;,&nbsp;f=&gt;{&nbsp;&nbsp;</span></span></li><li class=""""><span>&nbsp;&nbsp;&nbsp;&nbsp;f.ClassName=<span class=""string"">""form-inline""</span><span>;&nbsp;&nbsp;</span></span></li><li class=""alt""><span>&nbsp;&nbsp;&nbsp;&nbsp;<span class=""keyword"">new</span><span>&nbsp;InputText(f,&nbsp;input=&gt;{&nbsp;&nbsp;</span></span></li><li class=""""><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;input.ClassName=<span class=""string"">""input-small""</span><span>;&nbsp;&nbsp;</span></span></li><li class=""alt""><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;input.SetPlaceHolder(<span class=""string"">""Email""</span><span>);&nbsp;&nbsp;</span></span></li><li class=""""><span>&nbsp;&nbsp;&nbsp;&nbsp;});&nbsp;&nbsp;</span></li><li class=""alt""><span>&nbsp;&nbsp;&nbsp;&nbsp;<span class=""keyword"">new</span><span>&nbsp;InputPassword(f,&nbsp;input=&gt;{&nbsp;&nbsp;</span></span></li><li class=""""><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;input.ClassName=<span class=""string"">""input-small""</span><span>;&nbsp;&nbsp;</span></span></li><li class=""alt""><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;input.SetPlaceHolder(<span class=""string"">""Password""</span><span>);&nbsp;&nbsp;</span></span></li><li class=""""><span>&nbsp;&nbsp;&nbsp;&nbsp;});&nbsp;&nbsp;</span></li><li class=""alt""><span>&nbsp;&nbsp;&nbsp;&nbsp;<span class=""keyword"">new</span><span>&nbsp;Label(f,&nbsp;lb=&gt;{&nbsp;&nbsp;</span></span></li><li class=""""><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class=""keyword"">new</span><span>&nbsp;InputCheckbox(lb,&nbsp;cb=&gt;{});&nbsp;&nbsp;</span></span></li><li class=""alt""><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;lb.Append(<span class=""string"">""Remember&nbsp;me""</span><span>);&nbsp;&nbsp;</span></span></li><li class=""""><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;lb.ClassName=<span class=""string"">""checkbox""</span><span>;&nbsp;&nbsp;</span></span></li><li class=""alt""><span>&nbsp;&nbsp;&nbsp;&nbsp;});&nbsp;&nbsp;</span></li><li class=""""><span>&nbsp;&nbsp;&nbsp;&nbsp;<span class=""keyword"">new</span><span>&nbsp;SubmitButton(f,&nbsp;bt=&gt;{&nbsp;&nbsp;</span></span></li><li class=""alt""><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;bt.Text(<span class=""string"">""Sign&nbsp;in""</span><span>);&nbsp;&nbsp;</span></span></li><li class=""""><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;bt.JQuery().Click(ev=&gt;{ev.PreventDefault();});&nbsp;&nbsp;</span></li><li class=""alt""><span>&nbsp;&nbsp;&nbsp;&nbsp;});&nbsp;&nbsp;</span></li><li class=""""><span>});&nbsp;&nbsp;</span></li></ol><textarea style=""display:none;"" class=""originalCode"">new Form(div , f=&gt;{
+	f.ClassName=""form-inline"";
+	new InputText(f, input=&gt;{
+		input.ClassName=""input-small"";
+		input.SetPlaceHolder(""Email"");
+	});
+	new InputPassword(f, input=&gt;{
+		input.ClassName=""input-small"";
+		input.SetPlaceHolder(""Password"");
+	});
+	new Label(f, lb=&gt;{
+		new InputCheckbox(lb, cb=&gt;{});
+		lb.Append(""Remember me"");
+		lb.ClassName=""checkbox"";
+	});
+	new SubmitButton(f, bt=&gt;{
+		bt.Text(""Sign in"");
+		bt.JQuery().Click(ev=&gt;{ev.PreventDefault();});
+	});
+});</textarea></div>");
+			}).AppendTo(parent);
+
+			//--------------------------------
+
+			Document.CreateElement("h2").Text("Horizontal Form").AppendTo(parent);
+			
+			new Div(null, div=>{
+				div.ClassName="bs-docs-example";
+				new Form(div , f=>{
+					f.ClassName="form-horizontal";
+					new TextField(f, (label,input)=>{
+						label.Text("Email");
+						input.SetPlaceHolder("Email");
+					});
+					new TextField(f, (label,input)=>{
+						label.Text("Password");
+						input.Type="password";
+						input.SetPlaceHolder("Password");
+					});
+					new CheckboxField(f, (lb,cb)=>{
+						lb.Text("Remember me");
+						new SubmitButton(lb.ParentNode, bt=>{
+							bt.Text("Sign in");
+							bt.JQuery().Click(ev=>{ev.PreventDefault();});
+						});
+					});
+
+				});
+				div.Append(@"<div class=""dp-highlighter""><div class=""bar""><div class=""tools""><a href=""#"" onclick=""dp.sh.Toolbar.Command('ViewSource',this);return false;"">view plain</a><a href=""#"" onclick=""dp.sh.Toolbar.Command('CopyToClipboard',this);return false;"">copy to clipboard</a><a href=""#"" onclick=""dp.sh.Toolbar.Command('PrintSource',this);return false;"">print</a><a href=""#"" onclick=""dp.sh.Toolbar.Command('About',this);return false;"">?</a></div></div><ol start=""1"" class=""dp-c""><li class=""alt""><span><span class=""keyword"">new</span><span>&nbsp;Form(div&nbsp;,&nbsp;f=&gt;{&nbsp;&nbsp;</span></span></li><li class=""""><span>&nbsp;&nbsp;&nbsp;&nbsp;f.ClassName=<span class=""string"">""form-horizontal""</span><span>;&nbsp;&nbsp;</span></span></li><li class=""alt""><span>&nbsp;&nbsp;&nbsp;&nbsp;<span class=""keyword"">new</span><span>&nbsp;TextField(f,&nbsp;(label,input)=&gt;{&nbsp;&nbsp;</span></span></li><li class=""""><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;label.Text(<span class=""string"">""Email""</span><span>);&nbsp;&nbsp;</span></span></li><li class=""alt""><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;input.SetPlaceHolder(<span class=""string"">""Email""</span><span>);&nbsp;&nbsp;</span></span></li><li class=""""><span>&nbsp;&nbsp;&nbsp;&nbsp;});&nbsp;&nbsp;</span></li><li class=""alt""><span>&nbsp;&nbsp;&nbsp;&nbsp;<span class=""keyword"">new</span><span>&nbsp;TextField(f,&nbsp;(label,input)=&gt;{&nbsp;&nbsp;</span></span></li><li class=""""><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;label.Text(<span class=""string"">""Password""</span><span>);&nbsp;&nbsp;</span></span></li><li class=""alt""><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;input.Type=<span class=""string"">""password""</span><span>;&nbsp;&nbsp;</span></span></li><li class=""""><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;input.SetPlaceHolder(<span class=""string"">""Password""</span><span>);&nbsp;&nbsp;</span></span></li><li class=""alt""><span>&nbsp;&nbsp;&nbsp;&nbsp;});&nbsp;&nbsp;</span></li><li class=""""><span>&nbsp;&nbsp;&nbsp;&nbsp;<span class=""keyword"">new</span><span>&nbsp;CheckboxField(f,&nbsp;(lb,cb)=&gt;{&nbsp;&nbsp;</span></span></li><li class=""alt""><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;lb.Text(<span class=""string"">""Remember&nbsp;me""</span><span>);&nbsp;&nbsp;</span></span></li><li class=""""><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class=""keyword"">new</span><span>&nbsp;SubmitButton(lb.ParentNode,&nbsp;bt=&gt;{&nbsp;&nbsp;</span></span></li><li class=""alt""><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;bt.Text(<span class=""string"">""Sign&nbsp;in""</span><span>);&nbsp;&nbsp;</span></span></li><li class=""""><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;bt.JQuery().Click(ev=&gt;{ev.PreventDefault();});&nbsp;&nbsp;</span></li><li class=""alt""><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;});&nbsp;&nbsp;</span></li><li class=""""><span>&nbsp;&nbsp;&nbsp;&nbsp;});&nbsp;&nbsp;</span></li><li class=""alt""><span>});&nbsp;&nbsp;</span></li></ol><textarea style=""display:none;"" class=""originalCode"">new Form(div , f=&gt;{
+	f.ClassName=""form-horizontal"";
+	new TextField(f, (label,input)=&gt;{
+		label.Text(""Email"");
+		input.SetPlaceHolder(""Email"");
+	});
+	new TextField(f, (label,input)=&gt;{
+		label.Text(""Password"");
+		input.Type=""password"";
+		input.SetPlaceHolder(""Password"");
+	});
+	new CheckboxField(f, (lb,cb)=&gt;{
+		lb.Text(""Remember me"");
+		new SubmitButton(lb.ParentNode, bt=&gt;{
+			bt.Text(""Sign in"");
+			bt.JQuery().Click(ev=&gt;{ev.PreventDefault();});
+		});
+	});
+});</textarea></div>");
+			}).AppendTo(parent);
+
+
+			//--------------------------------
+
+			/*
+*/
 		}
 		
 	}
