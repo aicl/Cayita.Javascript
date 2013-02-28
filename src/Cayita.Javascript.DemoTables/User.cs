9df14ce5318bff95a23df0a3d1b75c7948b3a744
angel ignacio colmenares laguado <angel.ignacio.colmenares@gmail.com>
2013-02-28 15:53:51 -0500

    demo 9 : tables

--- src/Cayita.Javascript.DemoTables/User.cs
+++ src/Cayita.Javascript.DemoTables/User.cs
@@ -10,10 +10,7 @@ namespace Cayita.Javascript.DemoTables
 	[PreserveMemberCase]
 	public  class User
 	{
-
-		public User()
-		{
-		}
+		public User(){}
 		public int Id { get; set; }
 		public string Name { get; set; }
 		public string Address { get; set; }
@@ -25,7 +22,6 @@ namespace Cayita.Javascript.DemoTables
 		public string UserName { get; set; }
 		public string Password { get; set; }
 		public int Rating { get; set; }
-
 	}
 
 	[IgnoreNamespace]
@@ -39,7 +35,6 @@ namespace Cayita.Javascript.DemoTables
 					return u.DoB.ConvertToDate();
 				};
 			});
-
 		}
 	}
 }
