8f015e06d67b10e146aabf3bdf646072cf9afad4
angel colmenares <angel.ignacio.colmenares@gmail.com>
2013-04-15 15:27:27 -0500

    added: some missing files

---
+++ src/Cayita.Javascript.Demo/modules/DemoModals.js
@@ -0,0 +1,139 @@
+﻿(function() {
+	////////////////////////////////////////////////////////////////////////////////
+	// Modals.DemoModals
+	var $DemoModals = function() {
+	};
+	$DemoModals.execute = function(parent) {
+		$(parent).append(Cayita.UI.StringExtensions.header('Modals', 2));
+		(new Cayita.UI.Div.$ctor2(null, function(div) {
+			div.className = 'bs-docs-example';
+			$(div).append((new Cayita.UI.Button.$ctor2(div, function(b) {
+				$(b).text('Simple Modal Dialog I');
+				$(b).on('click', function(evt) {
+					Cayita.UI.Bootbox.dialog('Your App Text: cayita is awesome....', '<br/>');
+				});
+			})).element());
+			$(div).append('<div class="dp-highlighter"><div class="bar"><div class="tools"><a href="#" onclick="dp.sh.Toolbar.Command(\'ViewSource\',this);return false;">view plain</a><a href="#" onclick="dp.sh.Toolbar.Command(\'CopyToClipboard\',this);return false;">copy to clipboard</a><a href="#" onclick="dp.sh.Toolbar.Command(\'PrintSource\',this);return false;">print</a><a href="#" onclick="dp.sh.Toolbar.Command(\'About\',this);return false;">?</a></div></div><ol start="1" class="dp-c"><li class="alt"><span><span>Bootbox.Dialog(</span><span class="string">"Your&nbsp;App&nbsp;Text:&nbsp;cayita&nbsp;is&nbsp;awesome...."</span><span>);&nbsp;&nbsp;</span></span></li></ol><textarea style="display:none;" class="originalCode">Bootbox.Dialog("Your App Text: cayita is awesome....");</textarea></div>');
+		})).appendTo$1(parent);
+		(new Cayita.UI.Div.$ctor2(null, function(div1) {
+			div1.className = 'bs-docs-example';
+			$(div1).append((new Cayita.UI.Button.$ctor2(div1, function(b1) {
+				$(b1).text('Simple Modal Dialog II');
+				$(b1).on('click', function(evt1) {
+					Cayita.UI.Bootbox.dialog('Your App Text: cayita is awesome....', 'Cayita Dialog');
+				});
+			})).element());
+			$(div1).append('<div class="dp-highlighter"><div class="bar"><div class="tools"><a href="#" onclick="dp.sh.Toolbar.Command(\'ViewSource\',this);return false;">view plain</a><a href="#" onclick="dp.sh.Toolbar.Command(\'CopyToClipboard\',this);return false;">copy to clipboard</a><a href="#" onclick="dp.sh.Toolbar.Command(\'PrintSource\',this);return false;">print</a><a href="#" onclick="dp.sh.Toolbar.Command(\'About\',this);return false;">?</a></div></div><ol start="1" class="dp-c"><li class="alt"><span><span>Bootbox.Dialog(</span><span class="string">"Your&nbsp;App&nbsp;Text:&nbsp;cayita&nbsp;is&nbsp;awesome...."</span><span>,&nbsp;</span><span class="string">"Cayita&nbsp;Dialog"</span><span>)&nbsp;&nbsp;</span></span></li></ol><textarea style="display:none;" class="originalCode">Bootbox.Dialog("Your App Text: cayita is awesome....", "Cayita Dialog")</textarea></div>');
+		})).appendTo$1(parent);
+		(new Cayita.UI.Div.$ctor2(null, function(div2) {
+			div2.className = 'bs-docs-example';
+			$(div2).append((new Cayita.UI.Button.$ctor2(div2, function(b2) {
+				$(b2).text('Custom Dialog I');
+				$(b2).on('click', function(evt2) {
+					Cayita.UI.Bootbox.dialog$4('Message from App', function(opt) {
+						opt.header = 'Your Header Text';
+						opt.onEscape = function() {
+							Alertify.log.info('ESC pressed', 5000);
+						};
+					}, function(bt) {
+						var $t1 = Cayita.UI.DialogButton.$ctor();
+						$t1.label = 'OK';
+						$t1.class = 'btn btn-success';
+						$t1.callback = function() {
+							Alertify.log.info('OK clicked', 5000);
+						};
+						ss.add(bt, $t1);
+					});
+				});
+			})).element());
+			$(div2).append('<div class="dp-highlighter"><div class="bar"><div class="tools"><a href="#" onclick="dp.sh.Toolbar.Command(\'ViewSource\',this);return false;">view plain</a><a href="#" onclick="dp.sh.Toolbar.Command(\'CopyToClipboard\',this);return false;">copy to clipboard</a><a href="#" onclick="dp.sh.Toolbar.Command(\'PrintSource\',this);return false;">print</a><a href="#" onclick="dp.sh.Toolbar.Command(\'About\',this);return false;">?</a></div></div><ol start="1" class="dp-c"><li class="alt"><span><span>Bootbox.Dialog(</span><span class="string">"Message&nbsp;from&nbsp;App"</span><span>,opt=&gt;{&nbsp;&nbsp;</span></span></li><li class=""><span>&nbsp;&nbsp;&nbsp;&nbsp;opt.Header=<span class="string">"Your&nbsp;Header&nbsp;Text"</span><span>;&nbsp;&nbsp;</span></span></li><li class="alt"><span>&nbsp;&nbsp;&nbsp;&nbsp;opt.OnEscape=()=&gt;&nbsp;<span class="string">"ESC&nbsp;pressed"</span><span>.LogInfo();&nbsp;&nbsp;</span></span></li><li class=""><span>},&nbsp;&nbsp;</span></li><li class="alt"><span>bt=&gt;bt.Add(&nbsp;<span class="keyword">new</span><span>&nbsp;DialogButton(){&nbsp;&nbsp;</span></span></li><li class=""><span>&nbsp;&nbsp;&nbsp;&nbsp;Label=<span class="string">"OK"</span><span>,&nbsp;&nbsp;&nbsp;</span></span></li><li class="alt"><span>&nbsp;&nbsp;&nbsp;&nbsp;Class=<span class="string">"btn&nbsp;btn-success"</span><span>,&nbsp;&nbsp;</span></span></li><li class=""><span>&nbsp;&nbsp;&nbsp;&nbsp;Callback=&nbsp;()=&gt;&nbsp;<span class="string">"OK&nbsp;clicked"</span><span>.LogInfo()&nbsp;&nbsp;</span></span></li><li class="alt"><span>}));&nbsp;&nbsp;</span></li></ol><textarea style="display:none;" class="originalCode">Bootbox.Dialog("Message from App",opt=&gt;{\n\topt.Header="Your Header Text";\n\topt.OnEscape=()=&gt; "ESC pressed".LogInfo();\n},\nbt=&gt;bt.Add( new DialogButton(){\n\tLabel="OK", \n\tClass="btn btn-success",\n\tCallback= ()=&gt; "OK clicked".LogInfo()\n}));</textarea></div>');
+		})).appendTo$1(parent);
+		(new Cayita.UI.Div.$ctor2(null, function(div3) {
+			div3.className = 'bs-docs-example';
+			$(div3).append((new Cayita.UI.Button.$ctor2(div3, function(b3) {
+				$(b3).text('Custom Dialog II');
+				$(b3).on('click', function(evt3) {
+					var d = new Cayita.UI.Div.$ctor2(null, function(dd) {
+						dd.className = 'span3';
+						new Cayita.UI.TextField.$ctor1(dd, function(field) {
+							$(field).attr('placeholder', 'Your Name');
+						});
+						new Cayita.UI.CheckboxField.$ctor1(dd, 'I like cayita', function(cb) {
+							cb.checked = true;
+							cb.disabled = true;
+						});
+						new Cayita.UI.TextArea.$ctor2(dd, function(area) {
+							area.value = 'cayita is amazing ...';
+						});
+					});
+					Cayita.UI.Bootbox.dialog$1(d, function(opt1) {
+						opt1.header = 'large-modal';
+						opt1.classes = 'modal-large';
+						opt1.onEscape = function() {
+							Alertify.log.info('ESC pressed', 5000);
+						};
+					}, function(bt1) {
+						var $t2 = Cayita.UI.DialogButton.$ctor();
+						$t2.label = 'Cancel';
+						$t2.callback = function() {
+							Alertify.log.error('Cancel pressed. Click here to delete message', 0);
+						};
+						ss.add(bt1, $t2);
+						var $t3 = Cayita.UI.DialogButton.$ctor();
+						$t3.label = 'Save';
+						$t3.callback = function() {
+							Alertify.log.success('Save pressed', 5000);
+						};
+						ss.add(bt1, $t3);
+					});
+				});
+			})).element());
+			$(div3).append('<div class="dp-highlighter"><div class="bar"><div class="tools"><a href="#" onclick="dp.sh.Toolbar.Command(\'ViewSource\',this);return false;">view plain</a><a href="#" onclick="dp.sh.Toolbar.Command(\'CopyToClipboard\',this);return false;">copy to clipboard</a><a href="#" onclick="dp.sh.Toolbar.Command(\'PrintSource\',this);return false;">print</a><a href="#" onclick="dp.sh.Toolbar.Command(\'About\',this);return false;">?</a></div></div><ol start="1" class="dp-c"><li class="alt"><span><span>var&nbsp;d&nbsp;=&nbsp;</span><span class="keyword">new</span><span>&nbsp;Div(</span><span class="keyword">null</span><span>,&nbsp;dd=&gt;{&nbsp;&nbsp;</span></span></li><li class=""><span>&nbsp;&nbsp;&nbsp;&nbsp;dd.ClassName=<span class="string">"span3"</span><span>;&nbsp;&nbsp;</span></span></li><li class="alt"><span>&nbsp;&nbsp;&nbsp;&nbsp;<span class="keyword">new</span><span>&nbsp;TextField(dd,&nbsp;field=&gt;field.SetPlaceHolder(</span><span class="string">"Your&nbsp;Name"</span><span>));&nbsp;&nbsp;</span></span></li><li class=""><span>&nbsp;&nbsp;&nbsp;&nbsp;<span class="keyword">new</span><span>&nbsp;CheckboxField(dd,&nbsp;</span><span class="string">"I&nbsp;like&nbsp;cayita"</span><span>,&nbsp;cb=&gt;&nbsp;{&nbsp;cb.Checked=</span><span class="keyword">true</span><span>;&nbsp;cb.Disabled=</span><span class="keyword">true</span><span>;&nbsp;}&nbsp;);&nbsp;&nbsp;</span></span></li><li class="alt"><span>&nbsp;&nbsp;&nbsp;&nbsp;<span class="keyword">new</span><span>&nbsp;TextArea(dd,&nbsp;area=&gt;&nbsp;&nbsp;area.Value=</span><span class="string">"cayita&nbsp;is&nbsp;amazing&nbsp;..."</span><span>);&nbsp;&nbsp;</span></span></li><li class=""><span>});&nbsp;&nbsp;</span></li><li class="alt"><span>&nbsp;&nbsp;</span></li><li class=""><span>Bootbox.Dialog(d,&nbsp;opt=&gt;{&nbsp;&nbsp;</span></li><li class="alt"><span>&nbsp;&nbsp;&nbsp;&nbsp;opt.Header=<span class="string">"large-modal"</span><span>;&nbsp;&nbsp;</span></span></li><li class=""><span>&nbsp;&nbsp;&nbsp;&nbsp;opt.Classes=<span class="string">"modal-large"</span><span>;&nbsp;&nbsp;</span></span></li><li class="alt"><span>&nbsp;&nbsp;&nbsp;&nbsp;opt.OnEscape=&nbsp;()=&gt;&nbsp;<span class="string">"ESC&nbsp;pressed"</span><span>.LogInfo();&nbsp;&nbsp;</span></span></li><li class=""><span>},&nbsp;&nbsp;</span></li><li class="alt"><span>bt=&gt;{&nbsp;&nbsp;</span></li><li class=""><span>&nbsp;&nbsp;&nbsp;&nbsp;bt.Add(<span class="keyword">new</span><span>&nbsp;DialogButton{&nbsp;&nbsp;</span></span></li><li class="alt"><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Label=<span class="string">"Cancel"</span><span>,&nbsp;&nbsp;</span></span></li><li class=""><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Callback=()=&gt;&nbsp;<span class="string">"Cancel&nbsp;pressed.&nbsp;Click&nbsp;here&nbsp;to&nbsp;delete&nbsp;message"</span><span>.LogError()&nbsp;&nbsp;</span></span></li><li class="alt"><span>&nbsp;&nbsp;&nbsp;&nbsp;});&nbsp;&nbsp;</span></li><li class=""><span>&nbsp;&nbsp;&nbsp;&nbsp;bt.Add(&nbsp;<span class="keyword">new</span><span>&nbsp;DialogButton{&nbsp;&nbsp;</span></span></li><li class="alt"><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Label=<span class="string">"Save"</span><span>,&nbsp;&nbsp;</span></span></li><li class=""><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Callback=&nbsp;()=&gt;&nbsp;<span class="string">"Save&nbsp;pressed"</span><span>.LogSuccess()&nbsp;&nbsp;</span></span></li><li class="alt"><span>&nbsp;&nbsp;&nbsp;&nbsp;});&nbsp;&nbsp;</span></li><li class=""><span>});&nbsp;&nbsp;</span></li></ol><textarea style="display:none;" class="originalCode">var d = new Div(null, dd=&gt;{\n\tdd.ClassName="span3";\n\tnew TextField(dd, field=&gt;field.SetPlaceHolder("Your Name"));\n\tnew CheckboxField(dd, "I like cayita", cb=&gt; { cb.Checked=true; cb.Disabled=true; } );\n\tnew TextArea(dd, area=&gt;\tarea.Value="cayita is amazing ...");\n});\n\nBootbox.Dialog(d, opt=&gt;{\n\topt.Header="large-modal";\n\topt.Classes="modal-large";\n\topt.OnEscape= ()=&gt; "ESC pressed".LogInfo();\n},\nbt=&gt;{\n\tbt.Add(new DialogButton{\n\t\tLabel="Cancel",\n\t\tCallback=()=&gt; "Cancel pressed. Click here to delete message".LogError()\n\t});\n\tbt.Add( new DialogButton{\n\t\tLabel="Save",\n\t\tCallback= ()=&gt; "Save pressed".LogSuccess()\n\t});\n});</textarea></div>');
+		})).appendTo$1(parent);
+		(new Cayita.UI.Div.$ctor2(null, function(div4) {
+			div4.className = 'bs-docs-example';
+			$(div4).append((new Cayita.UI.Button.$ctor2(div4, function(b4) {
+				$(b4).text('Error Dialog ');
+				$(b4).on('click', function(evt4) {
+					Cayita.UI.Bootbox.error('You must use cayita', 'Error !!!');
+				});
+			})).element());
+			$(div4).append('<div class="dp-highlighter"><div class="bar"><div class="tools"><a href="#" onclick="dp.sh.Toolbar.Command(\'ViewSource\',this);return false;">view plain</a><a href="#" onclick="dp.sh.Toolbar.Command(\'CopyToClipboard\',this);return false;">copy to clipboard</a><a href="#" onclick="dp.sh.Toolbar.Command(\'PrintSource\',this);return false;">print</a><a href="#" onclick="dp.sh.Toolbar.Command(\'About\',this);return false;">?</a></div></div><ol start="1" class="dp-c"><li class="alt"><span><span>Bootbox.Error(</span><span class="string">"You&nbsp;must&nbsp;use&nbsp;cayita"</span><span>,&nbsp;</span><span class="string">"Error&nbsp;!!!"</span><span>)&nbsp;&nbsp;</span></span></li></ol><textarea style="display:none;" class="originalCode">Bootbox.Error("You must use cayita", "Error !!!")</textarea></div>');
+		})).appendTo$1(parent);
+		(new Cayita.UI.Div.$ctor2(null, function(div5) {
+			div5.className = 'bs-docs-example';
+			$(div5).append((new Cayita.UI.Button.$ctor2(div5, function(b5) {
+				$(b5).text('Alert ');
+				$(b5).on('click', function(evt5) {
+					bootbox.alert('User is over quota', 'OK', null);
+				});
+			})).element());
+			$(div5).append('<div class="dp-highlighter"><div class="bar"><div class="tools"><a href="#" onclick="dp.sh.Toolbar.Command(\'ViewSource\',this);return false;">view plain</a><a href="#" onclick="dp.sh.Toolbar.Command(\'CopyToClipboard\',this);return false;">copy to clipboard</a><a href="#" onclick="dp.sh.Toolbar.Command(\'PrintSource\',this);return false;">print</a><a href="#" onclick="dp.sh.Toolbar.Command(\'About\',this);return false;">?</a></div></div><ol start="1" class="dp-c"><li class="alt"><span><span>Bootbox.Alert(</span><span class="string">"User&nbsp;is&nbsp;over&nbsp;quota"</span><span>)&nbsp;&nbsp;</span></span></li></ol><textarea style="display:none;" class="originalCode">Bootbox.Alert("User is over quota")</textarea></div>');
+		})).appendTo$1(parent);
+		(new Cayita.UI.Div.$ctor2(null, function(div6) {
+			div6.className = 'bs-docs-example';
+			$(div6).append((new Cayita.UI.Button.$ctor2(div6, function(b6) {
+				$(b6).text('Prompt');
+				$(b6).on('click', function(evt6) {
+					bootbox.prompt('Your name?', 'Cancel', 'OK', function(r) {
+						Alertify.log.info('Your name is:' + r, 5000);
+					}, '');
+				});
+			})).element());
+			$(div6).append('<div class="dp-highlighter"><div class="bar"><div class="tools"><a href="#" onclick="dp.sh.Toolbar.Command(\'ViewSource\',this);return false;">view plain</a><a href="#" onclick="dp.sh.Toolbar.Command(\'CopyToClipboard\',this);return false;">copy to clipboard</a><a href="#" onclick="dp.sh.Toolbar.Command(\'PrintSource\',this);return false;">print</a><a href="#" onclick="dp.sh.Toolbar.Command(\'About\',this);return false;">?</a></div></div><ol start="1" class="dp-c"><li class="alt"><span><span>Bootbox.Prompt(</span><span class="string">"Your&nbsp;name?"</span><span>,&nbsp;callback:r=&gt;(</span><span class="string">"Your&nbsp;name&nbsp;is:"</span><span>+&nbsp;r).LogInfo())&nbsp;&nbsp;</span></span></li></ol><textarea style="display:none;" class="originalCode">Bootbox.Prompt("Your name?", callback:r=&gt;("Your name is:"+ r).LogInfo())</textarea></div>');
+		})).appendTo$1(parent);
+		(new Cayita.UI.Div.$ctor2(null, function(div7) {
+			div7.className = 'bs-docs-example';
+			$(div7).append((new Cayita.UI.Button.$ctor2(div7, function(b7) {
+				$(b7).text('Confirm');
+				$(b7).on('click', function(evt7) {
+					bootbox.confirm('Are you sure?', 'Cancel', 'OK', function(res) {
+						Alertify.log.info((res ? 'OK' : 'Cancel') + ' pressed', 5000);
+					});
+				});
+			})).element());
+			$(div7).append('<div class="dp-highlighter"><div class="bar"><div class="tools"><a href="#" onclick="dp.sh.Toolbar.Command(\'ViewSource\',this);return false;">view plain</a><a href="#" onclick="dp.sh.Toolbar.Command(\'CopyToClipboard\',this);return false;">copy to clipboard</a><a href="#" onclick="dp.sh.Toolbar.Command(\'PrintSource\',this);return false;">print</a><a href="#" onclick="dp.sh.Toolbar.Command(\'About\',this);return false;">?</a></div></div><ol start="1" class="dp-c"><li class="alt"><span><span>Bootbox.Confirm(</span><span class="string">"Are&nbsp;you&nbsp;sure?"</span><span>,&nbsp;callback:&nbsp;res=&gt;&nbsp;(res?</span><span class="string">"OK"</span><span>:</span><span class="string">"Cancel"</span><span>+</span><span class="string">"&nbsp;pressed"</span><span>).LogInfo()&nbsp;)&nbsp;&nbsp;</span></span></li></ol><textarea style="display:none;" class="originalCode">Bootbox.Confirm("Are you sure?", callback: res=&gt; (res?"OK":"Cancel"+" pressed").LogInfo() )</textarea></div>');
+		})).appendTo$1(parent);
+	};
+	ss.registerClass(global, 'DemoModals', $DemoModals);
+})();
---
+++ src/Cayita.Javascript.Demo/modules/DemoPanels.js
@@ -0,0 +1,249 @@
+﻿(function() {
+	////////////////////////////////////////////////////////////////////////////////
+	// Modals.DemoPanels
+	var $DemoPanels = function() {
+	};
+	$DemoPanels.execute = function(parent) {
+		$(parent).append(Cayita.UI.StringExtensions.header('Panels', 2));
+		(new Cayita.UI.Div.$ctor2(null, function(div) {
+			div.className = 'bs-docs-example';
+			div.style.paddingLeft = '40px';
+			$(div).append(Cayita.UI.Div.createContainer$1(div, function(cf) {
+				Cayita.UI.Div.createRow$1(cf, function(rf) {
+					new Cayita.UI.Div.$ctor2(rf, function(ld) {
+						ld.className = 'span5';
+						var apps = (new Cayita.UI.Panel()).caption('Apps').closable(false).resizable(false);
+						new Cayita.UI.Div(function(id) {
+							id.className = 'c-icons';
+							$(id).append('<style>img {height: 40px;}  .c-icon {height: 95px;}</style>');
+							var $t1 = $Modals_App.getApps();
+							for (var $t2 = 0; $t2 < $t1.length; $t2++) {
+								var app = { $: $t1[$t2] };
+								new Cayita.UI.Anchor.$ctor3(id, ss.mkdel({ app: app }, function(a) {
+									a.className = 'c-icon';
+									new Cayita.UI.Image.$ctor2(a, ss.mkdel({ app: this.app }, function(img) {
+										img.src = this.app.$.get_icon();
+										img.className = 'img-rounded';
+									}));
+									$(a).on('click', ss.mkdel({ app: this.app }, function(ev) {
+										ev.preventDefault();
+										Alertify.log.info(this.app.$.get_tittle(), 5000);
+									}));
+									new Cayita.UI.Span.$ctor2(a, ss.mkdel({ app: this.app }, function(sp) {
+										sp.className = 'c-icon-label';
+										sp.innerHTML = this.app.$.get_tittle();
+									}));
+								}));
+							}
+							apps.append$4(id);
+						});
+						apps.render(ld);
+						var pn = (new Cayita.UI.Panel()).caption('Demo Panel').closable(false).resizable(false).draggable(false);
+						new Cayita.UI.Button(function(bt) {
+							$(bt).text('Grey background');
+							$(bt).on('click', function(evt) {
+								pn.body().style.backgroundColor = 'grey';
+							});
+							pn.append$4(bt);
+						});
+						new Cayita.UI.Button(function(bt1) {
+							$(bt1).text('White background');
+							$(bt1).on('click', function(evt1) {
+								pn.body().style.backgroundColor = 'white';
+							});
+							pn.append$4(bt1);
+						});
+						new Cayita.UI.Button(function(bt2) {
+							$(bt2).text('Collapse');
+							$(bt2).on('click', function(evt2) {
+								pn.collapse();
+							});
+							pn.append$4(bt2);
+						});
+						pn.render(ld);
+					});
+					new Cayita.UI.Div.$ctor2(rf, function(ld1) {
+						ld1.className = 'span5';
+						var coyote = (new Cayita.UI.Panel()).caption('El Coyote').closable(false).render(ld1).draggable(false).resizable(false);
+						new Cayita.UI.Div(function(d) {
+							d.className = 'span2';
+							new Cayita.UI.Image.$ctor2(d, function(i) {
+								i.src = 'img/coyote.jpg';
+								i.style.height = '20%';
+							});
+							coyote.append$4(d);
+						});
+						new Cayita.UI.Div(function(d1) {
+							d1.className = 'span10';
+							$(d1).append('<i><b>El <a href=\'https://es.wikipedia.org/wiki/Coyote\' title=\'Coyote\' target=\'_blank\'>Coyote</a> y el <a href=\'https://es.wikipedia.org/wiki/Geococcyx_californianus\' title=\'Geococcyx californianus\' target=\'_blank\'>Correcaminos</a></b></i> (<i><b>Wile E. Coyote</b> and the <b>Road Runner</b></i>) son los personajes de una serie <a href=\'https://es.wikipedia.org/wiki/Estados_Unidos\' title=\'Estados Unidos\' target=\'_blank\'>estadounidense</a> de <a href=\'https://es.wikipedia.org/wiki/Dibujos_animados\' title=\'Dibujos animados\' target=\'_blank\'>dibujos animados</a> creada en el año de <a href=\'https://es.wikipedia.org/wiki/1949\' title=\'1949\' target=\'_blank\'>1949</a> por el animador <a href=\'https://es.wikipedia.org/wiki/Chuck_Jones\' title=\'Chuck Jones\' target=\'_blank\'>Chuck Jones</a> para <a href=\'https://es.wikipedia.org/wiki/Warner_Brothers\' title=\'Warner Brothers\' target=\'_blank\'>Warner Brothers</a>. Chuck Jones se inspiró para crear a estos personajes en un libro de <a href=\'https://es.wikipedia.org/wiki/Mark_Twain\' title=\'Mark Twain\' target=\'_blank\'>Mark Twain</a>, titulado <i>Roughin It</i>, en el que Twain denotaba que los coyotes hambrientos podrían cazar un correcaminos.<br/><a href=\'https://es.wikipedia.org/wiki/El_Coyote_y_el_Correcaminos\' title=\'Coyote\' target=\'_blank\'>El Coyote (wikipedia)</a> ');
+							coyote.append$4(d1);
+						});
+						var tbp = (new Cayita.UI.Panel()).caption('Table').closable(false).draggable(false).resizable(false);
+						new Cayita.UI.HtmlTable.$ctor1(function(t) {
+							t.className = 'table table-striped table-hover table-condensed';
+							new Cayita.UI.TableHeader.$ctor1(t, function(h) {
+								new Cayita.UI.TableRow.$ctor1(h, function(tr) {
+									new Cayita.UI.TableCell.$ctor3(tr, function(td) {
+										$(td).text('Title');
+									});
+									new Cayita.UI.TableCell.$ctor3(tr, function(td1) {
+										$(td1).text('URL');
+									});
+								});
+							});
+							var $t3 = $Modals_App.getApps();
+							for (var $t4 = 0; $t4 < $t3.length; $t4++) {
+								var app1 = { $: $t3[$t4] };
+								new Cayita.UI.TableRow.$ctor1(t, ss.mkdel({ app1: app1 }, function(tr1) {
+									new Cayita.UI.TableCell.$ctor3(tr1, ss.mkdel({ app1: this.app1 }, function(td2) {
+										$(td2).text(this.app1.$.get_tittle());
+									}));
+									new Cayita.UI.TableCell.$ctor3(tr1, ss.mkdel({ app1: this.app1 }, function(td3) {
+										$(td3).text(this.app1.$.get_icon());
+									}));
+								}));
+							}
+							tbp.append$4(t);
+						});
+						tbp.render(ld1);
+					});
+				});
+			}).element());
+			$(div).append('');
+		})).appendTo$1(parent);
+		(new Cayita.UI.Div.$ctor2(null, function(div1) {
+			div1.className = 'bs-docs-example';
+			var i1 = 1;
+			new Cayita.UI.Button.$ctor2(div1, function(bt3) {
+				$(bt3).text('Window I');
+				$(bt3).on('click', function(evt3) {
+					(new Cayita.UI.Panel()).caption('Window ' + (i1++).toString()).left((i1 * 5).toString() + 'px').top((i1 * 15).toString() + 'px').width('300px').height('100px').overlay(true).render(null);
+				});
+			});
+			$(div1).append('');
+		})).appendTo$1(parent);
+		(new Cayita.UI.Div.$ctor2(null, function(div2) {
+			div2.className = 'bs-docs-example';
+			new Cayita.UI.Button.$ctor2(div2, function(bt4) {
+				$(bt4).text('Window II');
+				$(bt4).on('click', function(evt4) {
+					(new Cayita.UI.Panel()).caption('Custom Close Icon and Handler').overlay(true).left('20px').top('200px').width('auto').closeIconClass('icon-th-large').closable(true).closeIconHandler(function(p) {
+						p.caption('Icon Close Changed !!! ');
+						p.closeIconClass('icon-remove-circle');
+						p.closeIconHandler(function(pn1) {
+							pn1.close();
+						});
+						p.height('400px');
+					}).onCloseHandler(function(p1) {
+						Alertify.log.info(Cayita.UI.StringExtensions.header('panel closed ', 3).outerHTML, 5000);
+					}).append$3(new Cayita.UI.Button(function(b) {
+						$(b).text('Click me');
+						b.style.width = '100%';
+						b.style.height = '100%';
+						$(b).on('click', function(ev1) {
+							Alertify.log.info('button clicked!!!', 5000);
+						});
+					})).render(null);
+				});
+			});
+			$(div2).append('');
+		})).appendTo$1(parent);
+		(new Cayita.UI.Div.$ctor2(null, function(div3) {
+			div3.className = 'bs-docs-example';
+			new Cayita.UI.Button.$ctor2(div3, function(bt5) {
+				$(bt5).text('Window III');
+				$(bt5).on('click', function(evt5) {
+					var error = new Cayita.UI.Paragraph.$ctor1(function(p2) {
+						p2.style.color = 'red';
+						$(p2).append((new Cayita.UI.Icon.$ctor1(function(i2) {
+							i2.className = 'icon-minus-sign';
+							i2.style.marginTop = '8px';
+							i2.style.marginRight = '8px';
+						})).element());
+						$(p2).append(' panel was closed ');
+					});
+					var pn2 = (new Cayita.UI.Panel()).caption('No Closable No Collapsible').overlay(true).left('30px').top('400px').width('auto').closable(false).collapsible(false).onCloseHandler(function(p3) {
+						error.logInfo(5000);
+					});
+					pn2.append$3(new Cayita.UI.Button(function(b1) {
+						$(b1).text('Click me');
+						b1.style.width = '100%';
+						b1.style.height = '100%';
+						$(b1).on('click', function(be) {
+							pn2.closable(true);
+							pn2.collapsible(true);
+							pn2.caption('Now Closable and Collapsible');
+							b1.disabled = true;
+						});
+					}));
+					pn2.render(null);
+				});
+			});
+			$(div3).append('');
+		})).appendTo$1(parent);
+		;
+	};
+	////////////////////////////////////////////////////////////////////////////////
+	// Modals.App
+	var $Modals_App = function() {
+		this.$1$TittleField = null;
+		this.$1$IconField = null;
+	};
+	$Modals_App.prototype = {
+		get_tittle: function() {
+			return this.$1$TittleField;
+		},
+		set_tittle: function(value) {
+			this.$1$TittleField = value;
+		},
+		get_icon: function() {
+			return this.$1$IconField;
+		},
+		set_icon: function(value) {
+			this.$1$IconField = value;
+		}
+	};
+	$Modals_App.getApps = function() {
+		var $t1 = [];
+		var $t2 = new $Modals_App();
+		$t2.set_tittle('Calculator');
+		$t2.set_icon('img/calculator.png');
+		ss.add($t1, $t2);
+		var $t3 = new $Modals_App();
+		$t3.set_tittle('Control Panel');
+		$t3.set_icon('img/control.png');
+		ss.add($t1, $t3);
+		var $t4 = new $Modals_App();
+		$t4.set_tittle('Firewall Settings');
+		$t4.set_icon('img/firewall.png');
+		ss.add($t1, $t4);
+		var $t5 = new $Modals_App();
+		$t5.set_tittle('Spreadsheet');
+		$t5.set_icon('img/calc.png');
+		ss.add($t1, $t5);
+		var $t6 = new $Modals_App();
+		$t6.set_tittle('Mail');
+		$t6.set_icon('img/mail.png');
+		ss.add($t1, $t6);
+		var $t7 = new $Modals_App();
+		$t7.set_tittle('Jack Sparrow Navigator');
+		$t7.set_icon('img/web.png');
+		ss.add($t1, $t7);
+		var $t8 = new $Modals_App();
+		$t8.set_tittle('MonoDevelop');
+		$t8.set_icon('img/monodevelop.png');
+		ss.add($t1, $t8);
+		var $t9 = new $Modals_App();
+		$t9.set_tittle('Tomboy');
+		$t9.set_icon('img/tomboy.png');
+		ss.add($t1, $t9);
+		var $t10 = new $Modals_App();
+		$t10.set_tittle('Skype');
+		$t10.set_icon('img/skype.png');
+		ss.add($t1, $t10);
+		var a = $t1;
+		return a;
+	};
+	ss.registerClass(global, 'DemoPanels', $DemoPanels);
+	ss.registerClass(global, 'Modals.App', $Modals_App);
+})();
