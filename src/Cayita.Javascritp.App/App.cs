39ee11ae7f8d370d3b1b1e1c5222cb68566399c4
angel colmenares <angel.ignacio.colmenares@gmail.com>
2013-04-25 19:30:52 -0500

    demo complete!

--- src/Cayita.Javascritp.App/App.cs
+++ src/Cayita.Javascritp.App/App.cs
@@ -60,8 +60,8 @@ namespace Cayita.Javascritp.App
 					a.OnClick(GoHomeClick);
 				});
 				nav.AddItem((l,a)=>{
-					a.Text("Licence");
-					a.OnClick(GoLicence);
+					a.Text("License");
+					a.OnClick(GoLicense);
 				});
 				nav.AddItem((l,a)=>{
 					a.Text("Contact");
@@ -252,19 +252,39 @@ http://www.saltarelle-compiler.com/getting-started</a></li>
 
 		}
 		
-		void GoLicence(jQueryEvent evt)
+		void GoLicense(jQueryEvent evt)
 		{
-			"Licence".LogInfo ();
+			evt.PreventDefault ();
+			Work.Empty ();
+			Work.Append (@"<div class=""well"">
+<p>Copyright AICL.</p>
+<p>Licensed under the Apache License, Version 2.0 (the ""License""); you may not use this work except in compliance with the License. You may obtain a copy of the License in the LICENSE file, or at:</p><p><a target=""_blank"" href=""http://www.apache.org/licenses/LICENSE-2.0"">http://www.apache.org/licenses/LICENSE-2.0</a></p><p>Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an ""AS IS"" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.</p></div>");
+
 		}
 		
 		void GoContact(jQueryEvent evt)
 		{
-			"Contact".LogInfo ();
+			evt.PreventDefault ();
+			Work.Empty ();
+			Work.Append (@"<div class=""well"">
+<p><a target=""_blank"" href=""https://github.com/angelcolmenares"">https://github.com/angelcolmenares</a>
+<p><a target=""_blank"" href=""https://github.com/aicl"">https://github.com/aicl</a>
+</p></div>");
+
 		}
 		
 		void GoAbout(jQueryEvent evt)
 		{
-			"About".LogInfo ();
+			evt.PreventDefault ();
+			Work.Empty ();
+			Work.Append (@"<div class=""well"">
+<p>Cayita is a library for building Webapps using C#  as base language and the Saltarelle compiler 
+<a href=""http://www.saltarelle-compiler.com"" target=""_blank"">
+http://www.saltarelle-compiler.com
+</a>
+</p>
+</p></div>");
+
 		}
 
 	}
