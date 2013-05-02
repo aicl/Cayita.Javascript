134aa7f4a1ebfa041fca99201b59c7e4f4c90199
angel colmenares <angel.ignacio.colmenares@gmail.com>
2013-05-02 18:42:06 -0500

    fileupload start

--- src/Cayita.Javascript/UI/FileUpload.cs
+++ src/Cayita.Javascript/UI/FileUpload.cs
@@ -1,31 +1,150 @@
 using System;
+using Cayita.UI;
+using System.Html;
 
 namespace Cayita
 {
-	public class FileUpload
+	public class FileUpload:Div
 	{
-		public FileUpload ()
+		FileUploadConfig cfg;
+
+		public FileUpload (Element parent):base(parent)
 		{
+			Init ( new FileUploadConfig());
 		}
+
+		public FileUpload(Element parent, Action<FileUploadConfig> config):base(parent)
+		{
+			var c = new FileUploadConfig ();
+			config.Invoke (c);
+			Init (c);
+		}
+
+		public FileUpload(Element parent, FileUploadConfig config):base(parent)
+		{
+			Init (config);
+		}
+
+		void Init(FileUploadConfig config )
+		{
+			cfg = config;
+
+			var e = Element ();
+			e.ClassName = "fileupload fileupload-new";
+			e.SetAttribute ("data-provides", "fileupload");
+			new Div ( d => {
+				d.ClassName="input-append";
+				new Div(d, inpt=>{
+					inpt.ClassName="uneditable-input span3";
+					new Icon(inpt, i=> i.ClassName="icon-file fileupload-exists");
+					new Span(inpt, s=> s.ClassName="fileupload-preview");
+				});
+
+				new Span(d, sp=>{
+					sp.ClassName="btn btn-file";
+					new Span(sp, sl=>{
+						new Icon(sl, i=>{
+							i.ClassName=cfg.SelectIconClass;
+						});
+						var t= Document.CreateElement("ctxt");
+						t.AppendTo(sl);
+						t.InnerHTML=cfg.SelectText??"";
+					});
+
+					new Span(sp, sl=>{
+						new Icon(sl, i=>{
+							i.ClassName=cfg.SelectIconClass;
+						});
+						var t= Document.CreateElement("ctxt");
+						t.AppendTo(sl);
+						t.InnerHTML=cfg.SelectText??"";
+					});
+
+					new Input(sp, i=>{
+						i.Type="file";
+						i.Name=cfg.Fieldname;
+					});
+				});
+
+				new Anchor(d, a=>{
+					a.ClassName="btn fileupload-exists";
+					a.SetAttribute("data-dismiss","fileupload");
+					var t= Document.CreateElement("ctxt");
+					t.AppendTo(a);
+					t.InnerHTML=cfg.RemoveText??"";
+				
+				});
+
+			}).AppendTo(e);
+		}
+
+
+
+	}
+
+	public class FileUploadConfig
+	{
+		public FileUploadConfig()
+		{
+			SelectIconClass = "icon-folder-open";
+			RemoveIconClass = "icon-remove";
+			Fieldname = "";
+
+		}
+
+		public string SelectText {get;set;}
+		public string SelectIconClass { get; set; }
+		public string RemoveText {get;set;}
+		public string RemoveIconClass { get; set; }
+		public string Fieldname { get; set; }
 	}
 }
 
-//http://markusslima.github.io/bootstrap-filestyle/ tomar api
+/*
+ * 
+ * 
+ * files: FileList Implementar File (y FileList ?);
+0: File
+lastModifiedDate: Mon Apr 29 2013 10:05:53 GMT-0500 (COT)
+name: "texture.png"
+size: 79876
+type: "image/png"
+webkitRelativePath: ""
+__proto__: File
+length: 1
+__proto__: FileList
+
+buttonText	String	Customization of button text.
+textField	Bool	Show and hide text field.
+classButton	String	Use of classes Bootstrap for button.
+classText	String	Use of classes Bootstrap for text field.
+icon	Bool	Use icon.
+classIcon	String	Use of classes Bootstrap for icons.
+Methods
+clear	String	Clears a set filestyl
+ */ 
+
+//http://markusslima.github.io/bootstrap-filestyle/  api ?
 // ver implementacion: http://blueimp.github.io/jQuery-File-Upload/
 // leer : http://duckranger.com/2012/06/pretty-file-input-field-in-bootstrap/
 
 // var f = document.getElementById("fu"); 
 // $f.on("change.fileupload", function(evt){console.log("change",evt)});
-// ver evt.target que es un input con el valor del archivo.
+// ver evt.target que es un input con el valor del archivo. ( es el input type="file")
+
+//https://developer.mozilla.org/en-US/docs/DOM/XMLHttpRequest/Using_XMLHttpRequest#Submitting_forms_and_uploading_files
+//https://developer.mozilla.org/en-US/docs/DOM/XMLHttpRequest/FormData/Using_FormData_Objects
 /*
 
 <input type="hidden" value="" name="">
-<form method="post" action="/api/admin/image" enctype="multipart/form-data">
+
+<form id="fu" method="post" action="/api/admin/image" enctype="multipart/form-data">
 <div class="fileupload fileupload-new" data-provides="fileupload">
 
 	<div class="fileupload-new thumbnail" style="width: 200px; height: 150px;"><img src="https://googledrive.com/host/0B5PxAJVNHVdKaGFMczUxX2RRSkk/img/form.demo-1.png"></div>
 	<div class="fileupload-preview fileupload-exists thumbnail" style="max-width: 200px; max-height: 150px; line-height: 20px;"></div>
-	<div>
+
+    <div>
 	  <span class="btn btn-file">
 		  <span class="fileupload-new">
 		  	<i class="icon-folder-open"></i>
@@ -35,7 +154,7 @@ namespace Cayita
 		  	<i class="icon-folder-open"></i>
 		  	<ctxt>Select</ctxt>
 		  </span>
-		  <input type="file" name="Filename">
+		  <input id="file" type="file" name="Filename">
 	  </span>
 	  <a href="#" class="btn fileupload-exists" data-dismiss="fileupload" title="remove">
 		  <input type="hidden" value="" name="">
@@ -44,6 +163,49 @@ namespace Cayita
 	  </a>
 	</div>
 </div>
-<input class="btn" type="submit" name="image" value="Upload" />
+<input class="btn" type="submit"  value="Upload" />
 </form>
+
+
+
+<div class="fileupload fileupload-exists" data-provides="fileupload">
+	<div class="input-append">
+    	<div class="uneditable-input span3">
+    		<i class="icon-file fileupload-exists"></i>
+    		<span class="fileupload-preview">texture.css</span>
+    	</div>
+    	<span class="btn btn-file">
+    		<span class="fileupload-new">Select file</span>
+    		<span class="fileupload-exists">Change</span>
+    		<input type="file" name="Filename">
+    	</span>
+    	<a href="#" class="btn fileupload-exists" data-dismiss="fileupload">Remove</a>
+    </div>
+</div>
+
+
+
+function sendForm2() {
+ 
+  var
+    oOutput = document.getElementById("output"),
+    oData = new FormData(document.getElementById("fu")); 
+
+ 
+  oData.append("CustomField", "This is some extra data");
+ 
+  var oReq = new XMLHttpRequest();
+  oReq.open("POST", "stash.php", true);
+  oReq.onload = function(oEvent) {
+    if (oReq.status == 200) {
+      oOutput.innerHTML = "Uploaded!";
+    } else {
+      oOutput.innerHTML = "Error " + oReq.status + " occurred uploading your file.<br \/>";
+    }
+  };
+ 
+  oReq.send(oData);
+ 
+}
+
 */
